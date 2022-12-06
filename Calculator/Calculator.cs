781f95bf9d667bd0ecf386da790ad2a2cab6348d
Dennypratamap <119923594+Dennypratamap@users.noreply.github.com>
2022-12-06 01:46:23 -0800

    Add files via upload

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+            CalculatorInit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void CalculatorInit()
+        {
+            comboBox1.Items.Clear();
+            comboBox1.Items.Add("Penjumlahan");
+            comboBox1.Items.Add("Pengurangan");
+            comboBox1.Items.Add("Perkalian");
+            comboBox1.Items.Add("Pembagian");
+            comboBox1.SelectedIndex = 0;
+        }
+
+        public delegate void Proses(int nilaiA, int nilaiB, string operasi, string operasiLabel, float hasil);
+
+        public event Proses prosesEvent;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnProses_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nilaiA = int.Parse(txtNilaiA.Text);
+            int nilaiB = int.Parse(txtNilaiB.Text);
+            string operasi = "";
+            string operasiLabel = "";
+            float hasil = 0;
+            switch (comboBox1.SelectedIndex)
+            {
+                // Penjumlahan
+                case 0:
+                    hasil = Hitung.Penjumlahan(nilaiA, nilaiB);
+                    operasiLabel = "Penjumlahan";
+                    operasi = "+";
+                    break;
+                // Pengurangan
+                case 1:
+                    hasil = Hitung.Pengurangan(nilaiA, nilaiB);
+                    operasiLabel = "Pengurangan";
+                    operasi = "-";
+                    break;
+                // Perkalian
+                case 2:
+                    hasil = Hitung.Perkalian(nilaiA, nilaiB);
+                    operasiLabel = "Perkalian";
+                    operasi = "x";
+                    break;
+                // Pembagian
+                case 3:
+                    hasil = (float)Hitung.Pembagian(nilaiA, nilaiB);
+                    operasiLabel = "Pembagian";
+                    operasi = "/";
+                    break;
+
+
+            }
+            prosesEvent(nilaiA, nilaiB, operasi, operasiLabel, hasil);
+        
+
+    }
+}
+}
---
+++ Calculator/Hasil perhitungan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void proses(int nilaiA, int nilaiB, string operasi, string operasiLabel, float hasil)
+        {
+            listBox.Items.Add(
+                String.Format($"Hasil {operasiLabel} dari {nilaiA} {operasi} {nilaiB} = ") +
+                String.Format(hasil % 1 == 0 ? "{0:0}" : "{0:0.00}", hasil)
+            );
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Calculator formCalculator = new Calculator();
+            formCalculator.prosesEvent += proses;
+            formCalculator.ShowDialog();
+        }
+    }
+}
